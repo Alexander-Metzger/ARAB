28a1b0ba08a8d80005d8c4544af0e0f9360ed0ee
TechMarxist <90161391+Alexander-Metzger@users.noreply.github.com>
2022-08-29 19:51:19 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,12 +10,14 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 sudo dnf config-manager --add-repo https://dl.jami.net/nightly/fedora_36/jami-nightly.repo
 
 #remove bloat
-sudo dnf remove kmahjongg kmines kmouth kolourpaint okular kcharselect gnome-abrt khelpcenter
+sudo dnf remove kmahjongg kmines kmouth kolourpaint okular kcharselect gnome-abrt khelpcenter kmail
 
 #install software
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo dnf install kbackup ktorrent lutris neovim mullvad-vpn libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths OpenAudible jami openshot blender cura tnftp hashcat
+sudo dnf install kbackup ktorrent lutris neovim mullvad-vpn libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths OpenAudible jami openshot blender cura tnftp hashcat audacity 
 
 #connect to mullvad 
 mullvad account login 3474947215549241
 mullvad obfuscation
+mullvad connect
+
