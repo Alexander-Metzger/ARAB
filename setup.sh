dcc6799b520c3d674cb1ad6dc6ea5fb50057e3d2
TechMarxist <90161391+Alexander-Metzger@users.noreply.github.com>
2022-08-29 19:32:31 +0200

    setup.sh
    The setup script that does most of the work.

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#update
+sudo dnf  update -y && sudo dnf upgrade -y
+
+#add repositories
+sudo dnf install \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo dnf config-manager --add-repo https://dl.jami.net/nightly/fedora_36/jami-nightly.repo
+
+#remove bloat
+sudo dnf remove kmahjongg kmines kmouth kolourpaint okular kcharselect gnome-abrt khelpcenter
+
+#install software
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo dnf install kbackup ktorrent lutris neovim mullvad-vpn libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths OpenAudible jami openshot blender cura tnftp hashcat
+
+#connect to mullvad 
+mullvad account login 3474947215549241
+mullvad obfuscation
