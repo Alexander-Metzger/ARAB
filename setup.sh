e28c580f3339dfaebebc16963e4d33eca6f5d93c
TechMarxist <90161391+Alexander-Metzger@users.noreply.github.com>
2022-09-28 14:50:07 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ sudo dnf  update -y && sudo dnf upgrade -y
 #add repositories
 sudo dnf install \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf \
+  install https://github.com/rpmsphere/noarch/raw/master/r/rpmsphere-release-36-1.noarch.rpm
+
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo dnf config-manager --add-repo https://dl.jami.net/nightly/fedora_36/jami-nightly.repo
 
@@ -16,7 +19,8 @@ sudo dnf remove kmahjongg kmines kmouth kolourpaint okular kcharselect gnome-abr
 wget https://github.com/mullvad/mullvadvpn-app/releases/download/2022.4/MullvadVPN-2022.4_x86_64.rpm
 wget https://github.com/openaudible/openaudible/releases/download/v3.5.9/OpenAudible_3.5.9_x86_64.rpm
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo dnf install krita gimp inkscape neovim libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths neofetch audacity ardour6 mpv openshot obs-studio ktorrent jami youtube-dl blender cura qemu virt-manager virt-backup kbackup mediawriter asunder xfburn tnftp hashcat sensors-detect openssh hddtemp qelectrotech gpredict rpmbuild -y
+sudo dnf install \
+  krita gimp inkscape neovim libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths neofetch audacity ardour6 mpv openshot obs-studio ktorrent jami youtube-dl blender cura qemu virt-manager virt-backup kbackup mediawriter asunder tnftp hashcat sensors-detect openssh hddtemp rpmbuild -y
 sudo dnf install OpenAudible_3.5.9_x86_64.rpm MullvadVPN-2022.4_x86_64.rpm -y
 
 #connect to mullvad 
