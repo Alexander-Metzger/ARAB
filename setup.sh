9e8c4991b25c37e9d71b31dd7b075b600a0bc81c
TechMarxist <90161391+Alexander-Metzger@users.noreply.github.com>
2022-10-01 16:39:30 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,12 @@ sudo dnf \
   install https://github.com/rpmsphere/noarch/raw/master/r/rpmsphere-release-36-1.noarch.rpm
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo dnf config-manager --add-repo https://dl.jami.net/nightly/fedora_36/jami-nightly.repo
-dnf config-manager --add-repo https://download.opensuse.org/repositories/home:jejb1:Element/Fedora_36/home:jejb1:Element.repo
+sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:jejb1:Element/Fedora_36/home:jejb1:Element.repo
+sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:/polslinux:/otpclient/Fedora_36/home:polslinux:otpc
+lient.repo
+
+
+
 sudo dnf update -y
 
 #remove bloat
@@ -21,14 +26,16 @@ wget https://github.com/mullvad/mullvadvpn-app/releases/download/2022.4/MullvadV
 wget https://github.com/openaudible/openaudible/releases/download/v3.5.9/OpenAudible_3.5.9_x86_64.rpm
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sudo dnf install \
-  inkscape icecat gimp krita audacity ardour6 mpv blender cura neovim libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths neofetch mpv openshot obs-studio ktorrent jami youtube-dl cura qemu virt-manager virt-backup kbackup mediawriter asunder hashcat openssh rpmbuild git bsdtar veracrypt lutris element-desktop vorta keepassxc y
+  inkscape icecat gimp krita audacity ardour6 mpv blender cura neovim libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths neofetch mpv openshot obs-studio ktorrent jami youtube-dl cura qemu virt-manager virt-backup virt-install mediawriter asunder hashcat openssh rpmbuild git bsdtar veracrypt lutris element-desktop vorta keepassxc otpclient -y
 sudo dnf install OpenAudible_3.5.9_x86_64.rpm MullvadVPN-2022.4_x86_64.rpm -y
+sudo dnf group install --with-optional virtualization
 
 #connect to mullvad 
 mullvad account login 
 mullvad connect
 
 #fetch
+sudo systemctl enable libvirtd
 clear
 sleep 2
 sudo systemctl reboot
