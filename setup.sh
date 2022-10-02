b0f3b2b50ab04ea20d7a200a201d76b4abd27694
TechMarxist <90161391+Alexander-Metzger@users.noreply.github.com>
2022-10-02 09:52:17 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,19 +14,17 @@ sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/ho
 sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:/polslinux:/otpclient/Fedora_36/home:polslinux:otpc
 lient.repo
 
-
-
 sudo dnf update -y
 
 #remove bloat
-sudo dnf remove kmahjongg kmines kmouth kolourpaint okular kcharselect gnome-abrt khelpcenter kmail kpat dragonplayer firefox -y
+sudo dnf remove kmahjongg kmines kmouth kolourpaint okular kcharselect gnome-abrt khelpcenter kmail kpat dragonplayer -y
 
 #install software
 wget https://github.com/mullvad/mullvadvpn-app/releases/download/2022.4/MullvadVPN-2022.4_x86_64.rpm
 wget https://github.com/openaudible/openaudible/releases/download/v3.5.9/OpenAudible_3.5.9_x86_64.rpm
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sudo dnf install \
-  inkscape icecat gimp krita audacity ardour6 mpv blender cura neovim libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths neofetch mpv openshot obs-studio ktorrent jami youtube-dl cura qemu virt-manager virt-backup virt-install mediawriter asunder hashcat openssh rpmbuild git bsdtar veracrypt lutris element-desktop vorta keepassxc otpclient icecat -y
+  inkscape icecat gimp krita audacity ardour6 mpv blender cura neovim libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths neofetch mpv openshot obs-studio ktorrent jami youtube-dl cura qemu virt-manager virt-backup virt-install mediawriter asunder hashcat openssh rpmbuild git bsdtar veracrypt lutris element-desktop vorta keepassxc otpclient -y
 sudo dnf install OpenAudible_3.5.9_x86_64.rpm MullvadVPN-2022.4_x86_64.rpm -y
 sudo dnf group install --with-optional virtualization
 
