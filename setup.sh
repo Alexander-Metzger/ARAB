63097ed17c333c23a11719b70eb5c321eec046c5
TechMarxist <90161391+Alexander-Metzger@users.noreply.github.com>
2022-10-05 20:04:45 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ wget https://github.com/mullvad/mullvadvpn-app/releases/download/2022.4/MullvadV
 wget https://github.com/openaudible/openaudible/releases/download/v3.5.9/OpenAudible_3.5.9_x86_64.rpm
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sudo dnf install \
-  inkscape icecat gimp krita audacity ardour6 mpv blender cura neovim libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths neofetch mpv openshot obs-studio ktorrent jami youtube-dl cura qemu virt-manager virt-backup virt-install mediawriter asunder hashcat openssh rpmbuild git bsdtar veracrypt lutris element-desktop vorta keepassxc otpclient -y
+  inkscape icecat gimp krita audacity ardour6 mpv blender cura neovim libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths neofetch mpv openshot obs-studio ktorrent jami youtube-dl cura qemu virt-manager virt-backup virt-install mediawriter asunder hashcat openssh rpmbuild git bsdtar veracrypt lutris element-desktop vorta keepassxc otpclient hplip hplip-gui -y
 sudo dnf install OpenAudible_3.5.9_x86_64.rpm MullvadVPN-2022.4_x86_64.rpm -y
 sudo dnf group install --with-optional virtualization
 
