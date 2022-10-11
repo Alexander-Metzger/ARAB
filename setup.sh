d62f322f398944e2360711003d689b1304ba6cbe
TechMarxist <90161391+Alexander-Metzger@users.noreply.github.com>
2022-10-11 18:08:13 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ wget https://github.com/openaudible/openaudible/releases/download/v3.5.9/OpenAud
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sudo dnf install \
   krita ardour6 blender neovim neofetch mpv openshot obs-studio jami youtube-dl cura qemu virt-manager virt-backup virt-install mediawriter asunder rpmbuild git bsdtar veracrypt lutris vorta keepassxc otpclient -y
-sudo dnf install hashcat openssh wireshark qbittorrent vche -y
+sudo dnf install hashcat openssh wireshark qbittorrent vche compton -y
 sudo dnf install libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths -y
 sudo dnf install OpenAudible_3.5.9_x86_64.rpm MullvadVPN-2022.4_x86_64.rpm -y
 sudo dnf group install --with-optional virtualization -y
