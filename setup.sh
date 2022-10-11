49f70de7fdc10018463f0e9beecbf60f9f6a45e4
TechMarxist <90161391+Alexander-Metzger@users.noreply.github.com>
2022-10-11 17:48:13 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,6 @@ sudo dnf \
   install https://github.com/rpmsphere/noarch/raw/master/r/rpmsphere-release-36-1.noarch.rpm
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo dnf config-manager --add-repo https://dl.jami.net/nightly/fedora_36/jami-nightly.repo
-sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:jejb1:Element/Fedora_36/home:jejb1:Element.repo
 sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:/polslinux:/otpclient/Fedora_36/home:polslinux:otpc
 lient.repo
 
@@ -24,7 +23,8 @@ wget https://github.com/mullvad/mullvadvpn-app/releases/download/2022.4/MullvadV
 wget https://github.com/openaudible/openaudible/releases/download/v3.5.9/OpenAudible_3.5.9_x86_64.rpm
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sudo dnf install \
-  inkscape icecat gimp krita audacity ardour6 mpv blender cura neovim libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths neofetch mpv openshot obs-studio ktorrent jami youtube-dl cura qemu virt-manager virt-backup virt-install mediawriter asunder hashcat openssh rpmbuild git bsdtar veracrypt lutris element-desktop vorta keepassxc otpclient hplip hplip-gui xdelta zenity xterm -y
+  krita ardour6 blender neovim libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths neofetch mpv openshot obs-studio jami youtube-dl cura qemu virt-manager virt-backup virt-install mediawriter asunder rpmbuild git bsdtar veracrypt lutris vorta keepassxc otpclient -y
+sudo dnf install hashcat openssh wireshark qbittorrent -y
 sudo dnf install OpenAudible_3.5.9_x86_64.rpm MullvadVPN-2022.4_x86_64.rpm -y
 sudo dnf group install --with-optional virtualization
 
@@ -35,5 +35,4 @@ mullvad connect
 #fetch
 sudo systemctl enable libvirtd
 clear
-sleep 2
-sudo systemctl reboot
+exit 2
