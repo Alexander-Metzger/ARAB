9747a08bd5f2191af9966dae44190a1c5964f767
TechMarxist <90161391+Alexander-Metzger@users.noreply.github.com>
2022-11-21 08:46:58 +0100

    Update and rename start.sh to arab.sh

--- arab.sh
+++ arab.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 #create some folders
-mkdir ~/.config/polybar
-mkdir ~/.config/nvim
-mkdir ~/.config/picom
-mkdir ~/.config/rofi
-mkdir ~/.config/ranger
+cp -avr 
+cp -avr rofi ~/.config/
 mkdir ~/Bilder/wallpaper
 mkdir ~/Dokumente/Schule
 mkdir ~/Bilder/Memes
 mkdir ~/Dokumente/manuals
 mkdir ~/Musik/Kommunismus
-mkdir ~/Musik/satire
+mkdir ~/Musik/Nerdy
+mkdir ~/Musik/Rock
 
 #add rpm-fusion
 sudo dnf install \
@@ -22,7 +20,7 @@ sudo dnf update -y
 
 #install good software
 sudo dnf install \
- ardour7 obs-studio git krita neovim picom rofi neofetch polybar ranger dunst mpv qbittorrent lutris keepassxc kitty cargo rust youtube-dl libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths blender foliate flameshot thunar-archive-plugin thunar-media-tags-plugin thunar-vcs-plugin thunar-volman gtkhash-thunar gtkhash ubertooth wireshark torbrowser-launcher dnf-plugins-core dnf-data rhythmbox mpc unclutter \
+ ardour7 obs-studio git krita neovim picom rofi neofetch polybar ranger dunst mpv qbittorrent lutris keepassxc kitty cargo rust youtube-dl libreoffice libreoffice-langpack-de libreoffice-langpack-en libreoffice-TexMaths blender foliate flameshot thunar-archive-plugin thunar-media-tags-plugin thunar-vcs-plugin thunar-volman gtkhash-thunar gtkhash ubertooth wireshark torbrowser-launcher dnf-plugins-core dnf-data rhythmbox mpc unclutter -y
 
  sudo dnf group install --with-optional virtualization -y
  sudo rpm --import https://static.geogebra.org/linux/office@geogebra.org.gpg.key 
