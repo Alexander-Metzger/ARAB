49823830c0ca1c2c9bd98f16c0a9a42551fd3bd7
TechMarxist <90161391+Alexander-Metzger@users.noreply.github.com>
2022-12-01 18:48:12 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,20 +2,16 @@
 
 #This script was created & tested for Fedora 37 KDE Plasma Spin and will be continously updated and optimized.
 
-#if [ "$EUID" -ne 0 ]
-#  then echo "Please run as root"
-#  exit
-#fi
-
 #create some folders
 mkdir ~/Bilder/wallpaper
-mkdir ~/Dokumente/Schule
+mkdir --parents ~/Dokumente/Schule/Englisch
+mkdir --parents ~/Dokumente/Schule/Deutsch
+mkdir --parents ~/Dokumente/Schule/Mathematik
 mkdir ~/Dokumente/ePub
-mkdir ~/Musik/Kommunismus
 mkdir ~/Projekte
 
 #remove KDE bloat
-sudo dnf remove -y konsole dragonplayer kaddressbook dnfdragora gwenview khelpcenter kmail kmouth sieveeditor kolourpaint korganizer
+sudo dnf remove -y konsole dragonplayer kaddressbook dnfdragora gwenview khelpcenter kmail kmouth sieveeditor kolourpaint korganizer  
 
 #add rpm-fusion
 sudo dnf install -y \
@@ -24,20 +20,21 @@ sudo dnf install -y \
 #update
 sudo dnf update -y
 
+#add keys
+sudo rpm --import https://static.geogebra.org/linux/office@geogebra.org.gpg.key 
+wget https://mullvad.net/media/mullvad-code-signing.asc
+gpg --import mullvad-code-signing.asc
+
 #install good software
 sudo dnf install -y \
- ardour7 obs-studio git krita neovim neofetch mpv ktorrent lutris keepassxc cargo rust youtube-dl libreoffice-langpack-en libreoffice-langpack-de libreoffice-langpack-es libreoffice-TexMaths blender foliate blender foliate wireshark torbrowser-launcher dnf-plugins-core kitty
-
+ minetest syncthing syncthing-tools ardour7 obs-studio git krita neovim neofetch mpv ktorrent lutris keepassxc cargo rust youtube-dl libreoffice-langpack-en libreoffice-langpack-de libreoffice-langpack-es libreoffice-TexMaths blender foliate blender foliate wireshark torbrowser-launcher dnf-plugins-core kitty fgrun
  sudo dnf group install --with-optional virtualization -y
- sudo rpm --import https://static.geogebra.org/linux/office@geogebra.org.gpg.key 
  sudo dnf install http://www.geogebra.net/linux/rpm/x86_64/geogebra-classic-6.0.659.0-202108101226.x86_64.rpm 
- wget --content-disposition https://mullvad.net/download/app/rpm/latest
+ wget --content-disposition https://mullvad.net/download/app/rpm/latest 
  sudo dnf install -y MullvadVPN-*.rpm
-sudo systemctl enable libvirtd
-sudo dnf config-manager --add-repo https://brave-browser-rpm-nightly.s3.brave.com/x86_64/
-sudo rpm --import https://brave-browser-rpm-nightly.s3.brave.com/brave-core-nightly.asc
-#sudo dnf install brave-browser-nightly
-
+ sudo systemctl enable libvirtd
+ sudo systemctl enable syncthing@$USER.service
+ 
 rm -rf MullvadVPN-*.rpm
 rm -rf geogebra-classic-*.rpm
 
