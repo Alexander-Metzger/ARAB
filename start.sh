020a3dee073fc70433e235404ba64602efcb6329
ReUN9 <90161391+Alexander-Metzger@users.noreply.github.com>
2022-12-28 08:37:47 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,6 +18,9 @@ mkdir ~/Bilder/wallpaper
 mkdir ~/Projekte
 mkdir ~/Projekte/git-clones
 
+#make nvim ide-like bc thats nice!
+git clone https://github.com/LunarVim/Neovim-from-scratch.git ~/.config/nvim
+
 #cp ff add-pns
 cp -rf extensions ~/.mozilla/firefox/*-release
 cp -rf prefs.js ~/.mozilla/firefox/*-release
@@ -41,7 +44,7 @@ sudo dnf update -y
 
 #install good software
 sudo dnf install -y --allowerasing \
-mangohud kleopatra syncthing bismuth ardour7 obs-studio git krita neovim neofetch newsboat mpv ktorrent lutris keepassxc cargo rust youtube-dl rpm-build libreoffice-langpack-de libreoffice-langpack-es libreoffice-TexMaths blender foliate blender foliate wireshark torbrowser-launcher kitty fgrun
+wl-clipboard mangohud kleopatra syncthing bismuth ardour7 obs-studio git krita neovim neofetch newsboat mpv ktorrent lutris keepassxc cargo rust youtube-dl rpm-build libreoffice-langpack-de libreoffice-langpack-es libreoffice-TexMaths blender foliate blender foliate wireshark torbrowser-launcher kitty fgrun
 sudo dnf copr enable taw/joplingit-clones
 
  sudo dnf install joplin 
